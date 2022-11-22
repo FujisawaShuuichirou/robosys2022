dcb77535233c0371e573506e6a387abafe652cf4
koppechan90 <s21C1104DW@s.chibakoudai.jp>
2022-11-22 19:53:58 +0900

    addition file

--- test.bash
+++ test.bash
@@ -1,27 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-FileCopyrightText: 2022 Shuichiro Fujisawa
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-	echo NG at Line $1
-	res=1
- }
-res=0
-
-### I/O TEST ###
-out=$(seq 5 | ./plus)
-
-[ "${out}" = 15.0 ] || ng ${LINENO}
-
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo | ./plus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-		    　 
-[ "$res" = 0 ] && echo OK
-exit $res
-
+seq 5 | ./plus

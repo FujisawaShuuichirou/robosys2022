161260b47697da8cee898b277c426a4cd9c1b52c
koppechan90 <s21C1104DW@s.chibakoudai.jp>
2022-11-22 20:04:09 +0900

    addition file

--- test.bash
+++ test.bash
@@ -2,4 +2,17 @@
 # SPDX-FileCopyrightText: 2022 Shuichiro Fujisawa
 # SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
